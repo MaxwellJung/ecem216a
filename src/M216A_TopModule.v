2f7151d682a0b53641d237fe5be270c2baadb098
Maxwell Jung <jungmaxwell@gmail.com>
2024-11-08 14:02:41 -0800

    output correct width with wrong latency

--- src/M216A_TopModule.v
+++ src/M216A_TopModule.v
@@ -103,33 +103,13 @@ module M216A_TopModule(
         end
     end
 
-    // register outputs
-    reg [7:0] index_x_o_reg;
-    reg [7:0] index_y_o_reg;
-    reg [3:0] strike_o_reg;
-    assign index_x_o = index_x_o_reg;
-    assign index_y_o = index_y_o_reg;
-    assign strike_o = strike_o_reg;
-
-    always @(posedge clk_i) begin
-        if(rst_i) begin
-            index_x_o_reg <= 8'b0;
-            index_y_o_reg <= 8'b0;
-            strike_o_reg <= 4'b0;
-        end else begin
-            strike_o_reg <= strike_o_reg + 1;
-            index_x_o_reg <= MAX_WIDTH;
-            index_y_o_reg <= MAX_HEIGHT;
-        end
-    end
-
     // create an array to track strip widths
     reg [7:0] strip_widths [12:0]; // 8 bits required to encode width in range [0,128], total 13 width registers
 
     wire [3:0] strip_id_h0; // 4 bits to encode strip ID in range [1,13]
     wire [3:0] strip_id_h1; // 4 bits to encode strip ID in range [1,13]
-    wire [3:0] zstrip_id_h0 = strip_id_h0 - 1; // 4 bits to encode zero-indexed strip ID (zstrip_id) in range [0,12]
-    wire [3:0] zstrip_id_h1 = strip_id_h1 - 1; // 4 bits to encode zero-indexed strip ID (zstrip_id) in range [0,12]
+    wire [3:0] strip_zid_h0 = strip_id_h0 - 1; // 4 bits to encode zero-indexed strip ID (strip_zid) in range [0,12]
+    wire [3:0] strip_zid_h1 = strip_id_h1 - 1; // 4 bits to encode zero-indexed strip ID (strip_zid) in range [0,12]
 
     // map height h to strip ID
     height_to_id hti_0 (
@@ -143,34 +123,57 @@ module M216A_TopModule(
     );
 
     // [TODO] compare widths of allowed strips and choose best one
-    reg [7:0] occupied_width_h0;
-    reg [7:0] occupied_width_h1;
-    wire [3:0] least_occupied_strip = (occupied_width_h0 <= occupied_width_h1) ? strip_id_h0 : strip_id_h1;
-    wire [3:0] least_occupied_zstrip = least_occupied_strip - 1; // 4 bits to encode zero-indexed strip ID (zstrip_id) in range [0,12]
-    wire [7:0] occupied_width = strip_widths[least_occupied_zstrip];
-
-    // register strips for comparison
-    always @(posedge clk_i) begin
-        if(rst_i) begin
-            occupied_width_h0 <= 4'b0;
-            occupied_width_h1 <= 4'b0;
-        end else if (counter == 1) begin
-            occupied_width_h0 <= strip_widths[zstrip_id_h0];
-            occupied_width_h1 <= strip_widths[zstrip_id_h1];
-        end
-    end
+    wire [7:0] occupied_width_h0 = strip_widths[strip_zid_h0];
+    wire [7:0] occupied_width_h1 = strip_widths[strip_zid_h1];
+    wire [3:0] least_occupied_strip_id = (occupied_width_h0 <= occupied_width_h1) ? strip_id_h0 : strip_id_h1;
+    reg  [3:0] least_occupied_strip_id_reg;
+    wire [3:0] least_occupied_strip_zid = least_occupied_strip_id - 1; // 4 bits to encode zero-indexed strip ID (strip_zid) in range [0,12]
+    wire [7:0] least_occupied_width = strip_widths[least_occupied_strip_zid];
+    reg  [7:0] occupied_width_before;
 
     // strip width logic
-    wire place_program = (occupied_width + width_i_reg) <= MAX_WIDTH && (counter == 2);
+    wire place_program = (least_occupied_width + width_i_reg) <= MAX_WIDTH;
 
     always @(posedge clk_i) begin
         if(rst_i) begin
+            least_occupied_strip_id_reg <= 4'b0;
+            occupied_width_before <= 8'b0;
             for (integer i = 0; i < 13; i=i+1) begin
                 strip_widths[i] <= 8'b0;
             end
-        end else if (place_program) begin
-            strip_widths[least_occupied_zstrip] <= strip_widths[least_occupied_zstrip] + width_i_reg;
-            // compute output index
+        end else if (counter == 1) begin
+            least_occupied_strip_id_reg <= least_occupied_strip_id;
+            if (place_program) begin
+                occupied_width_before <= least_occupied_width;
+                strip_widths[least_occupied_strip_zid] <= least_occupied_width + width_i_reg;
+            end
+        end
+    end
+
+    // register outputs
+    reg [7:0] index_x_o_reg;
+    reg [7:0] index_y_o_reg;
+    reg [3:0] strike_o_reg;
+    assign index_x_o = index_x_o_reg;
+    assign index_y_o = index_y_o_reg;
+    assign strike_o = strike_o_reg;
+
+    always @(posedge clk_i) begin
+        if(rst_i) begin
+            index_x_o_reg <= 8'b0;
+            index_y_o_reg <= 8'b0;
+            strike_o_reg <= 4'b0;
+        end else if (counter == 2) begin
+            if (place_program) begin
+                strike_o_reg <= 0;
+                index_x_o_reg <= occupied_width_before;
+                // [TODO] compute y position from strip id
+                index_y_o_reg <= least_occupied_strip_id_reg;
+            end else begin
+                strike_o_reg <= strike_o_reg + 1;
+                index_x_o_reg <= MAX_WIDTH;
+                index_y_o_reg <= MAX_HEIGHT;
+            end
         end
     end
 
@@ -190,9 +193,9 @@ module height_to_id (
             strip_id_o = -2*program_height_i+18; // strip ID 10,8,6,4
         end else begin // heights 8 and 13
             if (program_height_i == 8)
-                strip_id_o = 1; // default to strip ID 1; handle ID 2 later
+                strip_id_o = 4'bXXXX; // throw error; handle later
             else if (program_height_i == 13)
-                strip_id_o = 13; //default to strip ID 13; handel ID 11 and 12 later
+                strip_id_o = 4'bXXXX; // throw error; handle later
         end
     end
 endmodule
