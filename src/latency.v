be1ef9263456f882973e9e75231b717c12c38953
JUNG <maxwelljung@eeapps04.seas.ucla.edu>
2024-11-17 10:29:23 -0800

    Run and pass testbench on modelsim

--- src/latency.v
+++ src/latency.v
@@ -11,16 +11,17 @@ module latency #(
     input  wire [WIDTH-1:0] in,
     output wire [WIDTH-1:0] out
 );
-    reg [WIDTH-1:0] ff_chain [LENGTH-1:0];
-
+    reg [WIDTH-1:0] ff_chain [LENGTH-1:0];    
+    integer i;
+    
     always @(posedge clk) begin
         if(rst) begin
-            for (integer i = 0; i < LENGTH; i=i+1) begin
+            for (i = 0; i < LENGTH; i=i+1) begin
                 ff_chain[i] <= 8'b0;
             end
         end else begin
             ff_chain[0] <= in;
-            for (integer i = 1; i < LENGTH; i=i+1) begin
+            for (i = 1; i < LENGTH; i=i+1) begin
                 ff_chain[i] <= ff_chain[i-1];
             end
         end
