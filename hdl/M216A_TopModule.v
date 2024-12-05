b5785968273703c39d3a8131cc4159f750dcf0b4
Maxwell Jung <jungmaxwell@gmail.com>
2024-12-04 20:39:53 -0800

    revert counter

--- hdl/M216A_TopModule.v
+++ hdl/M216A_TopModule.v
@@ -28,11 +28,9 @@ module M216A_TopModule(
     reg [2:0] counter;
     always @(posedge clk_i) begin
         if(rst_i) begin
-            counter <= 2'd0;
+            counter <= 0;
         end else if (valid_program) begin
-            counter <= (counter >= CYCLES_PER_PROGRAM) ? 1 : counter + 1;
-        end else begin
-            counter <= 1;
+            counter <= (counter >= CYCLES_PER_PROGRAM - 1) ? 0 : counter + 1;
         end
     end
 
@@ -44,7 +42,7 @@ module M216A_TopModule(
         if(rst_i) begin
             width_i_reg <= 5'b0;
             height_i_reg <= 5'b0;
-        end else if (counter == 1) begin
+        end else if (counter == 0) begin
             width_i_reg <= width_i;
             height_i_reg <= height_i;
         end
@@ -68,7 +66,7 @@ module M216A_TopModule(
             strip_id_0_reg <= 4'b0;
             strip_id_1_reg <= 4'b0;
             strip_id_2_reg <= 4'b0;
-        end else if (counter == 2) begin
+        end else if (counter == 1) begin
             strip_id_0_reg <= strip_id_0;
             strip_id_1_reg <= strip_id_1;
             strip_id_2_reg <= strip_id_2;
@@ -104,7 +102,7 @@ module M216A_TopModule(
         if(rst_i) begin
             chosen_strip_id_reg <= 4'b0;
             chosen_strip_width_reg <= 8'b0;
-        end else if (counter == 3) begin
+        end else if (counter == 2) begin
             chosen_strip_id_reg <= chosen_strip_id;
             chosen_strip_width_reg <= chosen_strip_width;
         end
@@ -120,7 +118,7 @@ module M216A_TopModule(
             for (i = 0; i < 13; i=i+1) begin
                 strip_widths[i] <= 8'b0;
             end
-        end else if (counter == 4) begin
+        end else if (counter == 3) begin
             if (place_program) begin
                 strip_widths[chosen_strip_id - 1] <= new_width;
             end
@@ -144,7 +142,7 @@ module M216A_TopModule(
             index_x_o_reg <= 8'b0;
             index_y_o_reg <= 8'b0;
             strike_o_reg <= 4'b0;
-        end else if (counter == 4) begin
+        end else if (counter == 3) begin
             if (place_program) begin
                 index_x_o_reg <= chosen_strip_width_reg;
                 index_y_o_reg <= strip_y_position;
