1794cc60f8a2ecaf8e2ad80014560e269b16b45e
Maxwell Jung <40750924+MaxwellJung@users.noreply.github.com>
2024-12-06 00:38:10 -0800

    feat: clocked least_strip module (#8)

--- hdl/M216A_TopModule.v
+++ hdl/M216A_TopModule.v
@@ -21,41 +21,81 @@ module M216A_TopModule(
     localparam MAX_WIDTH = 128;
     localparam MAX_HEIGHT = 128;
     localparam CYCLES_PER_PROGRAM = 4;
+    localparam OUTPUT_LATENCY = 8;
 
     wire valid_program = (width_i > 0) && (height_i > 0);
+    wire output_valid;
+    latency #(
+        .LENGTH(OUTPUT_LATENCY),
+        .WIDTH(1)
+    ) latency_output_valid (
+        .clk(clk_i),
+        .rst(rst_i),
+        .in(valid_program),
+        .out(output_valid)
+    );
 
-    // Cycle counter from 0 to (CYCLES_PER_PROGRAM - 1)
+    wire active = valid_program | output_valid;
+
+    // counter cycles from 0 to (CYCLES_PER_PROGRAM - 1)
     reg [2:0] counter;
+    // program_cycle cycles from 1 to CYCLES_PER_PROGRAM
+    // only when the module is active
+    wire [2:0] program_cycle = counter + active;
     always @(posedge clk_i) begin
         if(rst_i) begin
             counter <= 0;
-        end else if (valid_program) begin
+        end else if (active) begin
             counter <= (counter >= CYCLES_PER_PROGRAM - 1) ? 0 : counter + 1;
         end
     end
 
     // Register inputs
-    reg [4:0] width_i_reg;
-    reg [4:0] height_i_reg;
+    reg [4:0] width_i_reg, height_i_reg;
 
     always @(posedge clk_i) begin
         if(rst_i) begin
             width_i_reg <= 5'b0;
             height_i_reg <= 5'b0;
-        end else if (counter == 0) begin
+        end else if (program_cycle == 1) begin
             width_i_reg <= width_i;
             height_i_reg <= height_i;
         end
     end
 
-    // Array of registers to track all 13 strip widths
-    reg [7:0] strip_widths [12:0];
+    // Add extra latency to input to satisfy total 8 clock cycle latency requirement
+    wire [4:0] delayed_width_i_reg, delayed_height_i_reg;
+    latency #(
+        .LENGTH(2),
+        .WIDTH(10)
+    ) latency_width_i (
+        .clk(clk_i),
+        .rst(rst_i),
+        .in({width_i_reg, height_i_reg}),
+        .out({delayed_width_i_reg, delayed_height_i_reg})
+    );
+    // hold width until needed later
+    wire [4:0] extra_delayed_width_i_reg;
+    latency #(
+        .LENGTH(1),
+        .WIDTH(5)
+    ) latency_delayed_width_i_reg (
+        .clk(clk_i),
+        .rst(rst_i),
+        .in(delayed_width_i_reg),
+        .out(extra_delayed_width_i_reg)
+    );
+
+    // Array of registers to track all 13 strip widths + extra register to store MAX_WIDTH
+    // width of strip i is tracked by strip_widths[i]
+    // strip_widths[0] = MAX_WIDTH
+    reg [7:0] strip_widths [13:0];
 
     // Map program height h to 3 potential strip IDs
     wire [3:0] strip_id_0, strip_id_1, strip_id_2;
-    reg  [3:0] strip_id_0_reg, strip_id_1_reg, strip_id_2_reg;
+    reg  [3:0] strip_id_0_reg, strip_id_1_reg, strip_id_2_reg; // 1 cycle latency
     program_height_to_id program_height_to_id_0 (
-        .program_height_i(height_i_reg),
+        .program_height_i(delayed_height_i_reg),
         .strip_id_0_o(strip_id_0),
         .strip_id_1_o(strip_id_1),
         .strip_id_2_o(strip_id_2)
@@ -66,7 +106,7 @@ module M216A_TopModule(
             strip_id_0_reg <= 4'b0;
             strip_id_1_reg <= 4'b0;
             strip_id_2_reg <= 4'b0;
-        end else if (counter == 1) begin
+        end else if (program_cycle == 4) begin
             strip_id_0_reg <= strip_id_0;
             strip_id_1_reg <= strip_id_1;
             strip_id_2_reg <= strip_id_2;
@@ -74,53 +114,52 @@ module M216A_TopModule(
     end
 
     // Choose best strip ID
-    wire [7:0] chosen_strip_width;
-    wire [3:0] chosen_strip_id;
-    // Registered versions of the wires above
-    reg  [7:0] chosen_strip_width_reg;
-    reg  [3:0] chosen_strip_id_reg;
+    wire [3:0] chosen_strip_id_reg;
+    wire [7:0] chosen_strip_width_reg;
 
-    least_strip least_strip_0 (
+    least_strip #(
+        .MAX_WIDTH(MAX_WIDTH)
+    ) least_strip_0 (
+        .clk_i(clk_i),
+        .rst_i(rst_i),
+
         .strip_id_0_i(strip_id_0_reg),
-        // Assign MAX_WIDTH to illegal strip ID (when ID = 0) so it never gets chosen
-        .strip_width_0_i((strip_id_0_reg == 0) ? MAX_WIDTH[7:0] : strip_widths[strip_id_0_reg - 1]),
+        .strip_width_0_i(strip_widths[strip_id_0_reg]),
 
         .strip_id_1_i(strip_id_1_reg),
-        // Assign MAX_WIDTH to illegal strip ID (when ID = 0) so it never gets chosen
-        .strip_width_1_i((strip_id_1_reg == 0) ? MAX_WIDTH[7:0] : strip_widths[strip_id_1_reg - 1]),
+        .strip_width_1_i(strip_widths[strip_id_1_reg]),
 
         .strip_id_2_i(strip_id_2_reg),
-        // Assign MAX_WIDTH to illegal strip ID (when ID = 0) so it never gets chosen
-        .strip_width_2_i((strip_id_2_reg == 0) ? MAX_WIDTH[7:0] : strip_widths[strip_id_2_reg - 1]),
+        .strip_width_2_i(strip_widths[strip_id_2_reg]),
 
-        .strip_id_o(chosen_strip_id),
-        .strip_width_o(chosen_strip_width)
+        // registered output
+        .strip_id_o(chosen_strip_id_reg),
+        .strip_width_o(chosen_strip_width_reg)
     );
 
-    // Register chosen strip id and width
+    wire [7:0] new_width = chosen_strip_width_reg + extra_delayed_width_i_reg;
+    reg  [7:0] new_width_reg;
     always @(posedge clk_i) begin
         if(rst_i) begin
-            chosen_strip_id_reg <= 4'b0;
-            chosen_strip_width_reg <= 8'b0;
-        end else if (counter == 2) begin
-            chosen_strip_id_reg <= chosen_strip_id;
-            chosen_strip_width_reg <= chosen_strip_width;
+            new_width_reg <= MAX_WIDTH;
+        end else if (program_cycle == 3) begin
+            new_width_reg <= new_width;
         end
     end
-
-    wire [7:0] new_width = chosen_strip_width_reg + width_i_reg;
-    wire place_program = new_width <= MAX_WIDTH;
+    wire place_program = (chosen_strip_id_reg != 0) & (new_width_reg <= MAX_WIDTH);
 
     // Add program to chosen strip and update its width
     integer i;
     always @(posedge clk_i) begin
         if(rst_i) begin
-            for (i = 0; i < 13; i=i+1) begin
+            for (i = 1; i <= 13; i=i+1) begin
                 strip_widths[i] <= 8'b0;
             end
-        end else if (counter == 3) begin
+            // Assign MAX_WIDTH to illegal strip ID (when ID = 0)
+            strip_widths[0] <= MAX_WIDTH[7:0];
+        end else if (program_cycle == 4) begin
             if (place_program) begin
-                strip_widths[chosen_strip_id - 1] <= new_width;
+                strip_widths[chosen_strip_id_reg] <= new_width_reg;
             end
         end
     end
@@ -142,11 +181,11 @@ module M216A_TopModule(
             index_x_o_reg <= 8'b0;
             index_y_o_reg <= 8'b0;
             strike_o_reg <= 4'b0;
-        end else if (counter == 3) begin
+        end else if (program_cycle == 4) begin
             if (place_program) begin
                 index_x_o_reg <= chosen_strip_width_reg;
                 index_y_o_reg <= strip_y_position;
-            end else begin
+            end else if (chosen_strip_id_reg != 0) begin
                 index_x_o_reg <= MAX_WIDTH;
                 index_y_o_reg <= MAX_HEIGHT;
                 strike_o_reg <= strike_o_reg + 1;
@@ -154,37 +193,8 @@ module M216A_TopModule(
         end
     end
 
-    // Add extra latency to output to satisfy total 8 clock cycle latency requirement
-    localparam extra_latency = 4;
-
-    latency #(
-        .LENGTH(extra_latency),
-        .WIDTH(8)
-    ) latency_x_o (
-        .clk(clk_i),
-        .rst(rst_i),
-        .in(index_x_o_reg),
-        .out(index_x_o)
-    );
-
-    latency #(
-        .LENGTH(extra_latency),
-        .WIDTH(8)
-    ) latency_y_o (
-        .clk(clk_i),
-        .rst(rst_i),
-        .in(index_y_o_reg),
-        .out(index_y_o)
-    );
-
-    latency #(
-        .LENGTH(extra_latency),
-        .WIDTH(4)
-    ) latency_strike_o (
-        .clk(clk_i),
-        .rst(rst_i),
-        .in(strike_o_reg),
-        .out(strike_o)
-    );
+    assign index_x_o = index_x_o_reg;
+    assign index_y_o = index_y_o_reg;
+    assign strike_o = strike_o_reg;
 
 endmodule
--- hdl/least_strip.v
+++ hdl/least_strip.v
@@ -1,33 +1,53 @@
 // least_strip.v
 // Given 3 strip IDs and their corresponding widths, output the strip ID with the smallest width
 // In case of tie, choose lower indexed ID/width (i.e. id/width_0 < id/width_1 < id/width_2)
-module least_strip (
+// IMPORTANT: Latency from input to output is 2 clock cycles (output is valid 2 clock cycles after input)
+module least_strip #(
+    parameter MAX_WIDTH = 128
+) (
+    input  wire       clk_i,
+    input  wire       rst_i,
+
     input  wire [3:0] strip_id_0_i,
-    input  wire  [7:0] strip_width_0_i,
+    input  wire [7:0] strip_width_0_i,
 
     input  wire [3:0] strip_id_1_i,
-    input  wire  [7:0] strip_width_1_i,
+    input  wire [7:0] strip_width_1_i,
     
     input  wire [3:0] strip_id_2_i,
-    input  wire  [7:0] strip_width_2_i,
+    input  wire [7:0] strip_width_2_i,
 
-    output reg  [3:0] strip_id_o,
-    output reg  [7:0] strip_width_o
+    output reg [3:0] strip_id_o,
+    output reg [7:0] strip_width_o
 );
+    reg id_0_smallest_reg, id_1_smallest_reg, id_2_smallest_reg;
 
-    always @(*) begin
-        if ((strip_width_0_i <= strip_width_1_i) && (strip_width_0_i <= strip_width_2_i)) begin
-            strip_id_o <= strip_id_0_i;
-            strip_width_o <= strip_width_0_i;
-        end else if ((strip_width_1_i <= strip_width_0_i) && (strip_width_1_i <= strip_width_2_i)) begin
-            strip_id_o <= strip_id_1_i;
-            strip_width_o <= strip_width_1_i;
-        end else if ((strip_width_2_i <= strip_width_0_i) && (strip_width_2_i <= strip_width_1_i)) begin
-            strip_id_o <= strip_id_2_i;
-            strip_width_o <= strip_width_2_i;
+    always @(posedge clk_i) begin
+        if(rst_i) begin
+            id_0_smallest_reg <= 1'b0;
+            id_1_smallest_reg <= 1'b0;
+            id_2_smallest_reg <= 1'b0;
+
+            strip_id_o <= 4'b0;
+            strip_width_o <= MAX_WIDTH;
         end else begin
-            strip_id_o <= 4'b0; // set to 4'b0 if not synthesizable
-            strip_width_o <= 8'b0; // set to 8'b11111111 if not synthesizable
+            id_0_smallest_reg <= (strip_width_0_i <= strip_width_1_i) && (strip_width_0_i <= strip_width_2_i);
+            id_1_smallest_reg <= (strip_width_1_i <= strip_width_0_i) && (strip_width_1_i <= strip_width_2_i);
+            id_2_smallest_reg <= (strip_width_2_i <= strip_width_0_i) && (strip_width_2_i <= strip_width_1_i);
+
+            if (id_0_smallest_reg) begin
+                strip_id_o <= strip_id_0_i;
+                strip_width_o <= strip_width_0_i;
+            end else if (id_1_smallest_reg) begin
+                strip_id_o <= strip_id_1_i;
+                strip_width_o <= strip_width_1_i;
+            end else if (id_2_smallest_reg) begin
+                strip_id_o <= strip_id_2_i;
+                strip_width_o <= strip_width_2_i;
+            end else begin
+                strip_id_o <= 4'b0; // set to 4'b0 if not synthesizable
+                strip_width_o <= MAX_WIDTH; // set to 8'b11111111 if not synthesizable
+            end
         end
     end
 
